e55ba9c611b5614f7590c4017d47b9caaf82cb0b
Aleksey <americos2007@yandex.ru>
2020-07-13 04:21:32 +0300

    add interface ITool

---
+++ GraphicRedactor000/GraphicRedactor000/Tool/ITool.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace GraphicRedactor000.Tool
+{
+   public interface ITool
+    {
+        void MouseDown(PictureBox drawArea, MouseEventArgs e);
+        void MouseUp(PictureBox drawArea, MouseEventArgs e);
+        void MouseMove(PictureBox drawArea, MouseEventArgs e);
+    }
+}
